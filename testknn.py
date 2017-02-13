262997d49bee778b55286cd408fb4e95e7fb4028
Daniel <dfsantosb@correo.udistrital.edu.co>
2017-02-13 10:54:36 -0500

    Modularize the project for being more open-closed.

--- testknn.py
+++ testknn.py
@@ -1,6 +1,46 @@
 from knn.from_scratch_knn import *
+import numpy as np
+from knn import plot as p
 
-if (__name__ == "__main__"):
+
+def test1():
+
+
+    data = [] 
+    sample = []
+    num = 25 #number of elements
+    # Feature set containing (x,y) values of 25 known/training data
+    #Create arrays of 25x1 of the range (0, 99)
+    x = np.random.randint(0,100,(num,1)).astype(np.float32)
+    y = np.random.randint(0,100,(num,1)).astype(np.float32)
+    
+    for i in range(0,num): 
+        sample.append([x[i][0], y[i][0]])
+
+    
+    arraynp = np.asarray(sample)
+
+    # Labels each one either Red or Blue with numbers 0 and 1
+    #Create array of data 25x1 fo the range 0,2
+    responses = np.random.randint(0,2,(num,1)).astype(np.float32)
+
+    """Example for doing something awesome
+    plt.scatter(blue[:,0],blue[:,1],80,'b','s')
+    """
+
+    
+    #print sample
+    #print arraynp
+    #print len(arraynp)
+
+    data.append(arraynp[responses.ravel() ==1 ])
+    data.append(arraynp[responses.ravel() ==0 ])
+    
+    p.plot(data)
+
+
+
+def test2():
 
     #All the sample
     data = load_data_set("datasets/flowers.csv")
@@ -10,6 +50,10 @@ if (__name__ == "__main__"):
 
     #The test set
     test_set = get_test_set(training_set, data) 
+
+
+    #This give me the kind of plants
+    kind_of_plants = data["Family"].unique()
     
     
 
@@ -37,3 +81,6 @@ if (__name__ == "__main__"):
             #print()
 
 
+
+if (__name__ == "__main__"):
+    test1()
