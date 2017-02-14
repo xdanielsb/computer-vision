f2cbb2b103d2963decfeae298b0df3fe3324a516
Daniel <dfsantosb@correo.udistrital.edu.co>
2017-02-13 21:18:12 -0500

    Check case of create an image of the same dimention.

--- knn_prediction/main.py
+++ knn_prediction/main.py
@@ -2,10 +2,11 @@ from knn_images import *
 
 if (__name__ =="__main__"):
     
-    factor = 2 ## x2 the image
+    factor = 1 ## x2 the image
     img1 = cv2.imread("../datasets/smile.jpg", 0)
     img2 = zoom_image(img1,factor)
-    knn(img1, img2, factor)
+    if  factor >=2:
+        knn(img1, img2, factor)
 
     cv2.imshow('image',img1)
     cv2.imshow('image2',img2)
