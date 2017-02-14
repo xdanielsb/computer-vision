936706b06067ff8415a37928584c1fcaf9130c9d
Daniel <dfsantosb@correo.udistrital.edu.co>
2017-02-14 18:01:52 -0500

    Create a readme for the project.

--- knn_prediction/main.py
+++ knn_prediction/main.py
@@ -2,7 +2,7 @@ from knn_images import *
 
 if (__name__ =="__main__"):
     
-    factor = 1 ## x2 the image
+    factor = 2## x2 the image
     img1 = cv2.imread("../datasets/smile.jpg", 0)
     img2 = zoom_image(img1,factor)
     if  factor >=2:
---
+++ knn_prediction/readme.md
@@ -0,0 +1,6 @@
+#ZOOM AN IMAGE IN CODE.
+##KNN - INTERPOLATION
+
+This a basic algorithm for filling the gaps when a image 
+is enlarge.
+
