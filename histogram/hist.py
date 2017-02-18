5e91d34f53e1b3dd8e505482e4ebc7a81f748860
Daniel <dfsantosb@correo.udistrital.edu.co>
2017-02-18 10:07:58 -0500

    Add images and improve method for finding the histogram.

--- histogram/hist.py
+++ histogram/hist.py
@@ -3,22 +3,18 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-img = cv2.imread('../images/hand1.jpg',  1)
 
-plt.figure(1)
-#Way one 
-hist = cv2.calcHist([img],[0],None,[256],[0,256])
-#Way two using numpy
-hist,bins = np.histogram(img.ravel(),256,[0,256])
+if(__name__=="__main__"):
+    img = cv2.imread('../assets/images/hand_1.jpg',  1)
+    plt.figure(1)
+    
+    plt.hist(img.ravel(),256,[0,256]); 
 
+    plt.figure(2)
 
-plt.hist(img.ravel(),256,[0,256]); 
-
-plt.figure(2)
-
-color = ('b','g','r')
-for i,col in enumerate(color):
-    histr = cv2.calcHist([img],[i],None,[256],[0,256])
-    plt.plot(histr,color = col)
-    plt.xlim([0,256])
-plt.show()
+    color = ('b','g','r')
+    for i,col in enumerate(color):
+        histr = cv2.calcHist([img],[i],None,[256],[0,256])
+        plt.plot(histr,color = col)
+        plt.xlim([0,256])
+    plt.show()
