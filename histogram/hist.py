a3bf6cb2d56888e8b7cd1731c96f0d8841361eb0
Daniel <dfsantosb@correo.udistrital.edu.co>
2017-02-19 18:37:44 -0500

    Add prewoit filter.

--- histogram/hist.py
+++ histogram/hist.py
@@ -2,19 +2,40 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+"""
+    Plot the histogram
+    img --> imagen
+    fig --> the number of the imagen
+"""
 
+"""
+    God link
+    http://stackoverflow.com/questions/6871201/plot-two-histograms-at-the-same-time-with-matplotlib
+"""
+
+def plot (img, fig, name):
+    bins = 256
+    range_scale = [0,254]
+
+   # plt.figure(fig)
+    plt.hist(img.ravel(),bins,range_scale, label=name, alpha=0.5);
+    plt.legend(loc='upper right')
+
+"""
+    Just the driver of the program
+"""
 
 if(__name__=="__main__"):
-    img = cv2.imread('../assets/images/hand_1.jpg',  1)
-    plt.figure(1)
+    name1 = "More Brigth"
+    name2 = "Bright"
+    name3 = "Less Bright"
+
+    img1 = cv2.imread('../assets/images/hand_1.jpg',  0)
+    img2 = cv2.imread('../assets/images/hand_2.jpg',  0)
+    img3 = cv2.imread('../assets/images/hand_3.jpg',  0)
     
-    plt.hist(img.ravel(),256,[0,256]); 
-
-    plt.figure(2)
-
-    color = ('b','g','r')
-    for i,col in enumerate(color):
-        histr = cv2.calcHist([img],[i],None,[256],[0,256])
-        plt.plot(histr,color = col)
-        plt.xlim([0,256])
+    plot(img1, 1, name2)
+    plot(img2, 2, name1)
+    plot(img3, 3, name3)
+    
     plt.show()
--- prewit/prewit.py
+++ prewit/prewit.py
@@ -1,5 +1,98 @@
 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
-#Read the image in gray scale
-img = cv2.imread("../datasets/smile.jpg", 0)
+
+def conv(img, kernel):
+    dst = cv2.filter2D(img,-1,kernel)
+    return dst
+
+
+def prewit(img):
+    N = np.matrix([[-1 ,-1 , -1], [1 ,-2 , 1], [1 , 1 ,1 ]], np.float32)
+    S = np.matrix([[1 ,1 , 1], [1 ,-2 , 1], [-1 , -1 ,-1 ]], np.float32)
+    E = np.matrix([[1 ,1 , -1], [1 ,-2 , -1], [1 , 1 ,-1 ]], np.float32)
+    O = np.matrix([[-1 ,1 , 1], [-1 ,-2 , 1], [-1 , 1 ,1 ]], np.float32)
+    SO = np.matrix([[1 ,1 , 1], [-1 ,-2 , 1], [-1 , -1 ,1 ]], np.float32)
+    NO = np.matrix([[-1 ,-1 , 1], [-1 ,-2 , 1], [1 , 1 ,1 ]], np.float32)
+    SE = np.matrix([[1 ,1 , 1], [1 ,-2 , -1], [1 , -1 ,-1 ]], np.float32)
+    NE = np.matrix([[1 ,-1 , -1], [1 ,-2 , -1], [1 , 1 ,1 ]], np.float32)
+
+    #Kernels
+    ms = [N,S,E,O,SO,NO,SE,NE]
+    
+
+
+
+    img2 = conv(img, N)
+    plt.subplot(3,3,2)
+    plt.imshow(img2)
+    plt.title('Conv with N')
+
+    img3 = conv(img, S)
+    plt.subplot(3,3,3)
+    plt.imshow(img3)
+    plt.title('Conv with S')
+
+    img4 = conv(img, E)
+    plt.subplot(3,3,4)
+    plt.imshow(img4)
+    plt.title('Conv with E')
+
+    img5 = conv(img, O)
+    plt.subplot(3,3,5)
+    plt.imshow(img5)
+    plt.title('Conv with O')
+
+    img6 = conv(img, SO)
+    plt.subplot(3,3,6)
+    plt.imshow(img6)
+    plt.title('Conv with SO')
+
+    img7 = conv(img, NO)
+    plt.subplot(3,3,7)
+    plt.imshow(img7)
+    plt.title('Conv with NO')
+
+    img8 = conv(img, SE)
+    plt.subplot(3,3,8)
+    plt.imshow(img8)
+    plt.title('Conv with SE')
+
+    img9 = conv(img, NE)
+    plt.subplot(3,3,9)
+    plt.imshow(img9)
+    plt.title('Conv with NE')
+
+
+    convs = [img2, img3, img4 , img5, img6, img7, img8, img9]
+
+
+    dimx, dimy = len(img2), len(img[0])
+    result = np.zeros(shape=(dimx,dimy), dtype=np.uint8)
+
+    for x in range(0,dimx):
+        for y in range(0,dimy):
+            result[x][y] = max(img2[x][y], img3[x][y], img4[x][y], img5[x][y], \
+                               img6[x][y], img7[x][y], img8[x][y], img9[x][y])
+                            
+
+    #plt.show()
+    cv2.imshow("Result", result)
+    cv2.imshow("Real Image", img)
+    #print(img9)
+    
+
+
+if (__name__ == "__main__"):
+
+    #Read the image in gray scale   
+    img = cv2.imread("../assets/images/smile.jpg", 0)
+   # cv2.imshow("Title",img)
+    prewit(img)
+    
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    
 
