08029d028347823b7f561f163e35e24971fd138c
xdanielsb <dfsantosbu@unal.edu.co>
2017-03-20 14:59:04 -0500

    Improve code hough transform.

--- hough/hough_transform.py
+++ hough/hough_transform.py
@@ -1,38 +1,79 @@
 import cv2
 import numpy as np
 
-imgc = cv2.imread('../assets/images/sudoku.png', 1)
-img = cv2.imread('../assets/images/sudoku.png', 0)
 
-th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)
-th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
 
-kernel = np.ones((3,3),np.uint8)
+import cv2
 
-edges = cv2.Canny(th2,50,150,apertureSize = 3)
+"""
+    This function read an image
+"""
+def readi(path, typer = "color"):
+    if typer == "color":
+        return cv2.imread(path, 1)
+    elif typer == "gray":
+        return cv2.imread(path, 0)
 
-maxo = 0
-ie = 0
-aux = []
 
-for i in range (140, 250):
-    lines = cv2.HoughLines(edges,1,np.pi/180,i)
-    aux.extend(lines)
-    
+"""
+    Show image for a certain time
+    time -> miliseconds
+    0 is an execption
+"""
+def time_show_image(time = 0):
+    #0 means, show the image indefenetely until any keypress
+    #25 means, show the image for 25 miliseconds
+    if time == 0:
+        print("\n\tPlease, press any key for finish the program")
+    cv2.waitKey(time)
 
-for l in aux:
-    for rho,theta in l:
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a*rho
-        y0 = b*rho
-        x1 = int(x0 + 1000*(-b))
-        y1 = int(y0 + 1000*(a))
-        x2 = int(x0 - 1000*(-b))
-        y2 = int(y0 - 1000*(a))
+"""
+    Close windows and de-allocate memory asociated with it.
+"""
+def close_windows():
+    cv2.destroyAllWindows()
 
-        cv2.line(imgc,(x1,y1),(x2,y2),(0,0,0),1)
 
-cv2.imshow("Hough transform", imgc)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def Hough_transform(img, imgc):
+
+
+    th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)
+    th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+
+    kernel = np.ones((3,3),np.uint8)
+
+    edges = cv2.Canny(th2,50,150,apertureSize = 3)
+
+    maxo = 0
+    ie = 0
+    aux = []
+
+    for i in range (140, 250):
+        lines = cv2.HoughLines(edges,1,np.pi/180,i)
+        aux.extend(lines)
+
+
+    for l in aux:
+        for rho,theta in l:
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a*rho
+            y0 = b*rho
+            x1 = int(x0 + 1000*(-b))
+            y1 = int(y0 + 1000*(a))
+            x2 = int(x0 - 1000*(-b))
+            y2 = int(y0 - 1000*(a))
+
+            cv2.line(imgc,(x1,y1),(x2,y2),(0,0,0),1)
+
+    cv2.imshow("Hough transform", imgc)
+
+
+
+if __name__ == "__main__":
+
+    imgc = readi('../assets/images/sudoku.png')
+    img = readi('../assets/images/sudoku.png', "gray")
+    Hough_transform(img,imgc)
+    time_show_image()
+    close_windows()
--- hough/probalistic_hough.py
+++ hough/probalistic_hough.py
@@ -1,19 +1,55 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('../assets/images/sudoku.png')
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray,50,150,apertureSize = 3)
+"""
+    This function read an image
+"""
+def readi(path, typer = "color"):
+    if typer == "color":
+        return cv2.imread(path, 1)
+    elif typer == "gray":
+        return cv2.imread(path, 0)
 
-lines = []
-for minLineLength in range (10, 100):
-    for maxLineGap in range (10,11):
-        lines.extend( cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap))
 
-for l in lines:
-    for x1,y1,x2,y2 in l:
-        cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+"""
+    Show image for a certain time
+    time -> miliseconds
+    0 is an execption
+"""
+def time_show_image(time = 0):
+    #0 means, show the image indefenetely until any keypress
+    #25 means, show the image for 25 miliseconds
+    if time == 0:
+        print("\n\tPlease, press any key for finish the program")
+    cv2.waitKey(time)
 
-cv2.imshow("Hough transform", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+"""
+    Close windows and de-allocate memory asociated with it.
+"""
+def close_windows():
+    cv2.destroyAllWindows()
+
+
+
+def Hough_transformlines(img):
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray,50,150,apertureSize = 3)
+
+    lines = []
+    for minLineLength in range (10, 100):
+        for maxLineGap in range (10,11):
+            lines.extend( cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap))
+
+    for l in lines:
+        for x1,y1,x2,y2 in l:
+            cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+
+    cv2.imshow("Hough transform", img)
+
+
+if __name__ == "__main__":
+
+    img = readi('../assets/images/sudoku.png')
+    Hough_transformlines(img)
+    time_show_image()
+    close_windows()
