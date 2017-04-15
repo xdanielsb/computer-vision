b8841ddcec33b434c3acb9062dff547c1712d2b5
xdanielsb <dfsantosbu@unal.edu.co>
2017-04-15 14:14:55 -0500

    Beautify code videocapture.

--- cpp/video/videocapture.cpp
+++ cpp/video/videocapture.cpp
@@ -41,71 +41,58 @@ void show_frame(string window_name, Mat frame){
     //blur
     blur( src_gray, detected_edges, Size(3,3) );
     
+
     //Now canny filter
-    int lowThreshold =0;
-    int const max_lowThreshold = 100;
+    int low =0;
+    int const max_lowThreshold= 100;
     int ratio  = 3;
     int kernel_size  = 3;
 
     /// Using Canny's output as a mask, we display our result
     dst = Scalar::all(0);
     frame.copyTo( dst, detected_edges);
+    
 
-    Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
+    Canny( detected_edges, detected_edges, low, low*ratio, kernel_size );
 
     imshow(window_name, detected_edges);
 }
 
-int process(VideoCapture& capture) {
+void Start() {
+    //Custom Video
+   // string arg ="/home/daniel/Videos/version2.mkv";
+    //Camera
+    string arg ="0";
+
+    VideoCapture capture(arg); //try to open string, this will attempt to open it as a video file or image sequence
+    if (!capture.isOpened()) //if this fails, try to open as a video camera, through the use of an integer param
+        capture.open(atoi(arg.c_str()));
+    if (!capture.isOpened()) {
+        cerr << "Failed to open the video device, video file or image sequence!\n" << endl;
+    }
     int n = 0;
     
-    
     cout << "Press space to save a picture. q or esc to quit" << endl;
-
     namedWindow(window_name, WINDOW_KEEPRATIO); //resizable window;
     //createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold );
-
     Mat frame;
 
     for (;;) {
         capture >> frame;
         if (frame.empty())
             break;
-
         show_frame(window_name, frame);
-        
         char key = (char)waitKey(30); //delay N millis, usually long enough to display and capture input
         options(key, frame);
-
     }
-    return 0;
+
 }
 
 
 
-void addTrackBar(int, void*){
-    
-      createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold );
-    
-}
 
-int main(int ac, char** av) {
-    
-    cv::CommandLineParser parser(ac, av, "{help h||}{@input||}");
-    std::string arg = parser.get<std::string>("@input");
-    if (arg.empty()) {
-        cout << "Write your options user." << endl;
-        return 1;
-    }
-    VideoCapture capture(arg); //try to open string, this will attempt to open it as a video file or image sequence
-    if (!capture.isOpened()) //if this fails, try to open as a video camera, through the use of an integer param
-        capture.open(atoi(arg.c_str()));
-    if (!capture.isOpened()) {
-        cerr << "Failed to open the video device, video file or image sequence!\n" << endl;
-        return 1;
-    }
-    process(capture);
-
+int main() {
+    Start();
     return 0;
 }
 
