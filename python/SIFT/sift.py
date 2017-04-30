9ff8741f904698a2a6b26276b1819b2b1ad7a6b3
xdanielsb <dfsantosbu@unal.edu.co>
2017-04-30 14:51:49 -0500

    Add sift algorithm.

--- python/SIFT/sift.py
+++ python/SIFT/sift.py
@@ -1,11 +1,11 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('../assets/images/hand_1.jpg')
+img = cv2.imread('../../tracking/python/video/0.png')
 
 
 #gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-gray = cv2.imread('../assets/images/hand.jpg')
+gray = cv2.imread('../../tracking/python/video/part2.png')
 sift = cv2.SIFT()
 kp = sift.detect(gray,None)
 img_s=cv2.drawKeypoints(gray,kp)
