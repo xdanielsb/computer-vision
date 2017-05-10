caaa9e7856ea3261625429e281573d27ea5306be
xdanielsb <dfsantosbu@unal.edu.co>
2017-05-09 22:30:34 -0500

    Add algorithm optical flow.

---
+++ python/opticalflow/main.py
@@ -0,0 +1,28 @@
+from numpy import * 
+import cv2
+#import numpy as np
+
+cap = cv2.VideoCapture(1)
+ret, frame1 = cap.read()
+prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+hsv = zeros_like(frame1)
+hsv[...,1] = 255
+while(1):
+    ret, frame2 = cap.read()
+    nextt = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+    
+    flow = cv2.calcOpticalFlowFarneback(prvs,nextt, 0.5, 3, 15, 3, 5, 1.2, 0)
+    mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+    hsv[...,0] = ang*180/pi/2
+    hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+    bgr = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+    cv2.imshow('frame2',bgr)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+    elif k == ord('s'):
+        cv2.imwrite('opticalfb.png',frame2)
+        cv2.imwrite('opticalhsv.png',bgr)
+    prvs = nextt
+cap.release()
+cv2.destroyAllWindows()
