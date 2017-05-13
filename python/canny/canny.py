448b03057d8682bbdb723558ad77114eed716091
xdanielsb <dfsantosbu@unal.edu.co>
2017-05-13 15:45:41 -0500

    Test canny with an image of an exam.

--- python/canny/canny.py
+++ python/canny/canny.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('../assets/images/sudoku.png',0)
+img = cv2.imread('../assets/images/exam.png',0)
 edges = cv2.Canny(img,100,200)
 plt.subplot(121),plt.imshow(img,cmap = 'gray')
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
